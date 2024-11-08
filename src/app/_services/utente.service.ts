75370ffe8ac79549029b9f0ea8720e939a959fcd
Jiplusplus <jiweikai03@icloud.com>
2024-11-08 15:45:04 +0100

    update

--- src/app/_services/utente.service.ts
+++ src/app/_services/utente.service.ts
@@ -20,7 +20,12 @@ export class UtenteService {
 
     this.refreshListEvent.pipe(
       switchMap(response => {
-        return this.httpClient.get(apiURL);
+        let jwt = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJlbjhTWG1KeVpteHlaOU81LUs3UE40SzdVRHB5MEJ1WVhJZmZZQ2wtQ0RVIn0.eyJleHAiOjE3MzEwNzM3MTIsImlhdCI6MTczMTA3MzQxMiwianRpIjoiZjkyMGIxYWUtNTU4Yy00N2E5LThmOGQtMTYwOWE4NmYyZDk2IiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDkwL3JlYWxtcy9hcGlfdXRlbnRpIiwiYXVkIjoiYWNjb3VudCIsInN1YiI6ImM2NTg3ODc1LTAwZDItNDNhZi1iYzJkLTE2MWY0YTIzYTk5YSIsInR5cCI6IkJlYXJlciIsImF6cCI6ImJhY2tlbmRfdXRlbnRpIiwic2lkIjoiMmU4ZDgwOWYtMzMwYi00ZmYzLTkzMjgtZDRmYjFiZWVlZDRhIiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyIvKiJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsiZGVmYXVsdC1yb2xlcy1hcGlfdXRlbnRpIiwib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoib3BlbmlkIGVtYWlsIHByb2ZpbGUiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwibmFtZSI6InVzZXIgdW5vIiwicHJlZmVycmVkX3VzZXJuYW1lIjoidXNlcjEiLCJnaXZlbl9uYW1lIjoidXNlciIsImZhbWlseV9uYW1lIjoidW5vIiwiZW1haWwiOiJ1c2VyMUBnbWFpbC5jb20ifQ.JtRuOdAMSw8Gw4fzqXG1-CqODOUiO3WVsNfF9P142WkQMboBJqu2FcXA4TgzGnw1UI56ZdJOhaUCXC3LTIzu3zE2FAWwh3c4EJwRPy1jRO2NR2qhRqwhjgrvg3ru_AmXJZ3bEMPBndnRUkq__9qdqZkS3e7uvaaIT0yhHePpoemDj5xnj0cIIBEnKNgDFtJsj9Ad4dYPZ4zb9sDSPTqGJx7Xjh8LL1L0K4JrRMPEpDE94yQBJ9r_kvoV5BJpG1gPd1r6GYUK8fu-1Vdbb0gF_GB7eaZGXR4uXf9f4wYmHSsnBpDfa4OCf3AeTRsLRPYQcbVW55UGrVknEzA5CrXJXw';
+        return this.httpClient.get(apiURL, {
+          headers:{
+            'Authorization':`Bearer ${jwt}`
+          }
+        });
       })
     ).subscribe(e => {
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { FormEditingComponent } from './form-editing/form-editing.component';
 import { ListaComponent } from './lista/lista.component';
 import { FormComponent } from './form/form.component';
+import { LoginComponent } from './login/login.component';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
     {path: '', pathMatch: 'full', redirectTo: '/editing'},
-    {path: 'editing', component: FormEditingComponent},
-    {path: 'form', component: FormComponent},
-    {path: 'lista', component: ListaComponent},
+    {path: 'editing', component: FormEditingComponent, canActivate: [authGuard]},
+    {path: 'form', component: FormComponent, canActivate: [authGuard]},
+    {path: 'lista', component: ListaComponent, canActivate: [authGuard]},
+    {path:'login', component: LoginComponent},
 ];
